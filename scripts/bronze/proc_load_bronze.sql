6e5009d3c426bcde35ac46c5a0165b5dc97d660f
itsyashk1406 <yashkamath.yk@gmail.com>
2025-08-07 23:28:44 +0530

    UPDATE the stored_procedure for loading the bronze layer

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -7,6 +7,7 @@ Script Purpose:
     It performs the following actions:
     - Truncates the bronze tables before loading data.
     - Uses the `BULK INSERT` command to load data from csv Files to bronze tables.
+	- Update the filepath of your datasets before running the stored procedure.
 
 Parameters:
     None. 
@@ -19,13 +20,14 @@ Usage Example:
 
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
-	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME; 
+	DECLARE @start_time DATETIME, @end_time DATETIME, @batch_start_time DATETIME, @batch_end_time DATETIME ,
+	@filePath NVARCHAR(500) = '-- INSERT FILE PATH HERE --' , @fullPath NVARCHAR(1000);
 	BEGIN TRY
 	SET @batch_start_time = GETDATE();
 		PRINT '================================================';
 		PRINT 'Loading Bronze Layer';
 		PRINT '================================================';
-
+		PRINT '                                                ';
 		PRINT '------------------------------------------------';
 		PRINT 'Loading CRM Tables';
 		PRINT '------------------------------------------------';
@@ -34,8 +36,9 @@ BEGIN
 		PRINT '>> Truncating Table: bronze.crm_cust_info';
 		TRUNCATE TABLE bronze.crm_cust_info;
 		PRINT '>> Inserting Data Into: bronze.crm_cust_info';
+		SET @fullPath=@filePath + 'sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
 		BULK INSERT bronze.crm_cust_info
-		FROM 'C:\Users\Acer\Downloads\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+		FROM @fullPath
 		WITH(
 		FIRSTROW=2,
 		FIELDTERMINATOR=',',
@@ -49,8 +52,9 @@ BEGIN
 		PRINT '>> Truncating Table: bronze.crm_prd_info';
 		TRUNCATE TABLE bronze.crm_prd_info;
 		PRINT '>> Inserting Data Into: bronze.crm_prd_info';
+		SET @fullPath=@filePath + 'sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
 		BULK INSERT bronze.crm_prd_info
-		FROM 'C:\Users\Acer\Downloads\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+		FROM @fullPath 
 		WITH(
 		FIRSTROW=2,
 		FIELDTERMINATOR=',',
@@ -64,8 +68,9 @@ BEGIN
 		PRINT '>> Truncating Table: bronze.crm_sales_details';
 		TRUNCATE TABLE bronze.crm_sales_details;
 		PRINT '>> Inserting Data Into: bronze.crm_sales_details';
+		SET @fullPath=@filePath + 'sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
 		BULK INSERT bronze.crm_sales_details
-		FROM 'C:\Users\Acer\Downloads\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+		FROM @fullPath
 		WITH(
 		FIRSTROW=2,
 		FIELDTERMINATOR=',',
@@ -83,8 +88,9 @@ BEGIN
 		PRINT '>> Truncating Table: bronze.erp_cust_az12';
 		TRUNCATE TABLE bronze.erp_cust_az12;
 		PRINT '>> Inserting Data Into: bronze.erp_cust_az12';
+		SET @fullPath=@filePath + 'sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
 		BULK INSERT bronze.erp_cust_az12
-		FROM 'C:\Users\Acer\Downloads\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+		FROM @fullPath 
 		WITH(
 		FIRSTROW=2,
 		FIELDTERMINATOR=',',
@@ -98,8 +104,9 @@ BEGIN
 		PRINT '>> Truncating Table: bronze.erp_loc_a101'
 		TRUNCATE TABLE bronze.erp_loc_a101;
 		PRINT '>> Inserting Data Into: bronze.erp_loc_a101';
+		SET @fullPath=@filePath + 'sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
 		BULK INSERT bronze.erp_loc_a101
-		FROM 'C:\Users\Acer\Downloads\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+		FROM @fullPath 
 		WITH(
 		FIRSTROW=2,
 		FIELDTERMINATOR=',',
@@ -113,8 +120,9 @@ BEGIN
 		PRINT '>> Truncating Table: bronze.erp_px_cat_g1v2';
 		TRUNCATE TABLE bronze.erp_px_cat_g1v2;
 		PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
+		SET @fullPath=@filePath  + 'sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
 		BULK INSERT bronze.erp_px_cat_g1v2
-		FROM 'C:\Users\Acer\Downloads\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+		FROM @fullPath
 		WITH(
 		FIRSTROW=2,
 		FIELDTERMINATOR=',',
